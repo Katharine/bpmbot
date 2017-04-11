d8677ba89c21768c4da03b25bd2f6598904664a9
Katharine Berry <katharine@kathar.in>
2017-04-10 21:34:43 -0700

    Now with added heroku.

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-PORT = 8081
-TELEGRAM_TOKEN = ""
-CACHE_TIME = 10
-MY_URL = "https://5e64928c.ngrok.io/"
+from os import environ as _environ
+
+PORT = int(_environ.get('PORT', 8081))
+TELEGRAM_TOKEN = _environ['TELEGRAM_TOKEN']
+CACHE_TIME = int(_environ.get('CACHE_TIME', 10))
+MY_URL = _environ.get("MY_URL", "https://5e64928c.ngrok.io/")
