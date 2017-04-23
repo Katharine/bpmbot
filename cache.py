f5b5448f16e6d1d5b527aea051694b20cc0426c6
Katharine Berry <katharine@kathar.in>
2017-04-23 11:25:29 -0700

    More cleanup.

--- cache.py
+++ cache.py
@@ -30,7 +30,6 @@ def cache_sticker(emote, flags):
         if emote in _cached:
             return _cached[emote]
     scale = max(1, settings.STICKER_MIN_SIZE / max(emote['size']))
-    print((settings.MY_URL + "/emote/{}-{}@{}x.webp").format(emote['name'], flag_str, scale))
     result = requests.post(
         "https://api.telegram.org/bot{}/sendSticker".format(settings.TELEGRAM_TOKEN),
         headers={"Content-Type": "application/json"},
